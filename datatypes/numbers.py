dc4d08420d6b9b95421c8fc783b4394cf6bc8304
rdharavath-evoke <rdharavath@evoketechnologies.com>
2021-06-25 13:45:48 +0530

    python-core-concepts

---
+++ datatypes/numbers.py
@@ -0,0 +1,19 @@
+var=123  #This will create a number integer assignment
+print(var)
+var='john'  #The 'var' now string type
+print(var)
+
+#The 'type' function returns information about how your data is stored within a variable
+message="good morning"
+num=55
+pi=3.14159
+print(type(message))  #this will return a string
+print(type(num))   #this will return an integer
+print(type(pi))    #this will return a float
+
+#python is automatically convert a number from one type to another 
+var1=5
+var2=6.6
+print(var1+var2)
+
+
---
+++ datatypes/string.py
@@ -0,0 +1,15 @@
+firstName='john'  #Python uses single quotes
+print(firstName)
+lastName="smith"  #Python uses double quotes and
+print(lastName)
+message="""this is a string that will span across multiple lines"""  #Python uses triple quotes also
+print(message)
+
+
+#Strings can be accessed as a whole string, or a Substring of the complete variable using brackets'[]'
+
+var1="hello world"
+print(var1[3])
+var2='GoPython'
+print(var2[3:7])
+
