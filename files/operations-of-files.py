980f2a04cde1f5ee16e462c545bffcde6f2e343a
rdharavath-evoke <rdharavath@evoketechnologies.com>
2021-07-08 12:52:19 +0530

    python-core-concepts

--- files/operations-of-files.py
+++ files/operations-of-files.py
@@ -23,3 +23,24 @@ try:
     print(f.read())
 except:
     print("file not exist")
+
+#### Program to show various ways to read and write data in a file
+
+file1=open("D:/myfile.txt","w")
+a=["This is Hyderabad\n","This is Mumbai\n","This is Delhi"]
+file1.write("hello Python\n")
+file1.writelines(a)
+file1.close()
+
+file1=open("D:/myfile.txt","r+")
+print("Output of read function is")
+print(file1.read())
+
+# o/p : 
+'''
+Output of the read function is
+hello Python
+This is Hyderabad
+This is Mumbai
+This is Delhi
+'''
